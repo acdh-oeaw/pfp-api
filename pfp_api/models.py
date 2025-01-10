5d92b2c55b70858e8f7763fd46802e797a1f9731
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-01-10 12:30:48 +0100

    feat(models): introduce events

--- pfp_api/models.py
+++ pfp_api/models.py
@@ -4,6 +4,10 @@ from pydantic import BaseModel, Field
 class Entity(BaseModel): ...
 
 
+class Event(Entity):
+    label: str = None
+
+
 class Person(Entity):
     person: str = None
     label: str = Field(..., example="Arthur Schnitzler")
