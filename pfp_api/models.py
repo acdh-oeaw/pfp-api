9d5cab00163ff46007fb33d15b98d9567d3a4d3b
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-02-11 15:59:39 +0100

    feat: group by provided entity
    resolves #13
    
    Uses provided entity to group events from different datasets
    to create a stream of events for one person

--- pfp_api/models.py
+++ pfp_api/models.py
@@ -1,21 +1,45 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import Annotated
+from rdfproxy import ConfigDict, SPARQLBinding
+from pydantic.networks import HttpUrl
 
 
 class Entity(BaseModel): ...
 
+class RelatedEntity(Entity):
+    model_config = ConfigDict(group_by="id")
+    id: Annotated[HttpUrl | None, SPARQLBinding("related_entity")] = None
+    label: Annotated[str | None, SPARQLBinding("relatedEntityLabel")] = None
+
+class RelatedPlace(Entity):
+    model_config = ConfigDict(group_by="id")
+    id: Annotated[HttpUrl | None, SPARQLBinding("related_place")] = None
+    label: Annotated[str | None, SPARQLBinding("relatedPlaceLabel")] = None
+    long: Annotated[float | None, SPARQLBinding("longitude")] = None
+    lat: Annotated[float | None, SPARQLBinding("latitude")] = None
 
 class Event(Entity):
-    label: str = None
+    model_config = ConfigDict(group_by="id")
+    id: Annotated[str, SPARQLBinding("event_id")]
+    label: Annotated[str, SPARQLBinding("eventLabel")]
+    startDate: str | None = None
+    endDate: str | None = None
+    relatedEntity: list[RelatedEntity]
+    relatedPlace: RelatedPlace
 
+
+class EntityProxy(Entity):
+    id: Annotated[HttpUrl, SPARQLBinding("proxy")]
+    label: str = Field(..., example="Arthur Schnitzler")
 
 class Person(Entity):
-    person: str = None
-    label: str = Field(..., example="Arthur Schnitzler")
-    graph: str = None
+    model_config = ConfigDict(group_by="id")
+    id: Annotated[HttpUrl, SPARQLBinding("pfp_uri")]
+    proxies: list[EntityProxy]
 
 
 class PersonDetail(Entity):
-    person: str = None
-    label: str = Field(..., example="Arthur Schnitzler")
-    events: Optional[List[Event]] = None
+    model_config = ConfigDict(group_by="id")
+    id: Annotated[str, SPARQLBinding("prov_entity")]
+    #label: Annotated[str, SPARQLBinding()]
+    events: list[Event]
